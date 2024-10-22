84b61733e27cbf17d6a25b874dc77b22793c4d88
ahmed.othman <Ahmed.Othman@eg.aictec.com>
2024-10-22 13:22:26 +0300

    test

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+Route::get('get-main-settings', [\App\Http\Controllers\apis\GeneralController::class, 'getMainSetting']);
 
 Route::middleware(['language','currency'])->group(function () {
-    Route::get('get-main-settings', [\App\Http\Controllers\apis\GeneralController::class, 'getMainSetting']);
     Route::get('home', [\App\Http\Controllers\apis\HomePageController::class, 'index']);
     Route::get('search', [\App\Http\Controllers\apis\HomePageController::class, 'search']);
     Route::get('brands', [\App\Http\Controllers\apis\BrandController::class, 'index']);
