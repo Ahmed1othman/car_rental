279cae61efa34d411a054309bb45825a208a5e39
ahmed.othman <Ahmed.Othman@eg.aictec.com>
2024-10-22 07:18:20 +0300

    test

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('home',[\App\Http\Controllers\apis\HomePageController::class,'index']);
-Route::get('search',[\App\Http\Controllers\apis\HomePageController::class,'search']);
-Route::get('brands',[\App\Http\Controllers\apis\BrandController::class,'index']);
-Route::get('categories',[\App\Http\Controllers\apis\CategoryController::class,'index']);
-Route::get('locations',[\App\Http\Controllers\apis\LocationController::class,'index']);
-Route::get('cars',[\App\Http\Controllers\apis\CarController::class,'index']);
-Route::get('blogs',[\App\Http\Controllers\apis\BlogController::class,'index']);
+Route::middleware(['language','currency'])->group(function () {
+    Route::get('home', [\App\Http\Controllers\apis\HomePageController::class, 'index']);
+    Route::get('search', [\App\Http\Controllers\apis\HomePageController::class, 'search']);
+    Route::get('brands', [\App\Http\Controllers\apis\BrandController::class, 'index']);
+    Route::get('categories', [\App\Http\Controllers\apis\CategoryController::class, 'index']);
+    Route::get('locations', [\App\Http\Controllers\apis\LocationController::class, 'index']);
+    Route::get('cars', [\App\Http\Controllers\apis\CarController::class, 'index']);
+    Route::get('blogs', [\App\Http\Controllers\apis\BlogController::class, 'index']);
+});
