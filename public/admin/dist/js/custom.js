a56d8205c7014aa81e73923b671b44a63f72d5fe
ahmed.othman <Ahmed.Othman@eg.aictec.com>
2025-01-04 23:01:10 +0200

    resolve delete

--- public/admin/dist/js/custom.js
+++ public/admin/dist/js/custom.js
@@ -125,53 +125,53 @@
             }
         });
     });
-//
-//     // Delete Action with SweetAlert Confirmation
-//     $('.delete-btn').on('click', function() {
-//         var id = $(this).data('id');
-//         var model = $(this).data('model');
-//         var deleteUrl = '/admin/' + model + '/' + id; // Adjust the delete route dynamically
-//
-//         Swal.fire({
-//             title: 'Are you sure?',
-//             text: 'You won\'t be able to revert this!',
-//             icon: 'warning',
-//             showCancelButton: true,
-//             confirmButtonColor: '#3085d6',
-//             cancelButtonColor: '#d33',
-//             confirmButtonText: 'Yes, delete it!',
-//         }).then((result) => {
-//             if (result.isConfirmed) {
-//                 $.ajax({
-//                     url: deleteUrl,
-//                     method: 'POST',
-//                     data: {
-//                         _token: $('meta[name="csrf-token"]').attr('content'), // Use CSRF token dynamically
-//                         _method: 'DELETE'
-//                     },
-//                     success: function(response) {
-//                         Swal.fire({
-//                             icon: 'success',
-//                             title: 'Deleted!',
-//                             text: 'The record has been deleted.',
-//                             timer: 2000,
-//                             showConfirmButton: false
-//                         });
-//
-//                         // Optionally reload or remove the row from the table
-//                         location.reload();
-//                     },
-//                     error: function(xhr) {
-//                         Swal.fire({
-//                             icon: 'error',
-//                             title: 'Error',
-//                             text: 'Something went wrong. Please try again later.',
-//                         });
-//                     }
-//                 });
-//             }
-//         });
-//     });
+
+    // Delete Action with SweetAlert Confirmation
+    $('.delete-btn').on('click', function() {
+        var id = $(this).data('id');
+        var model = $(this).data('model');
+        var deleteUrl = '/admin/' + model + '/' + id; // Adjust the delete route dynamically
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'You won\'t be able to revert this!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: deleteUrl,
+                    method: 'POST',
+                    data: {
+                        _token: $('meta[name="csrf-token"]').attr('content'), // Use CSRF token dynamically
+                        _method: 'DELETE'
+                    },
+                    success: function(response) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Deleted!',
+                            text: 'The record has been deleted.',
+                            timer: 2000,
+                            showConfirmButton: false
+                        });
+
+                        // Optionally reload or remove the row from the table
+                        location.reload();
+                    },
+                    error: function(xhr) {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Error',
+                            text: 'Something went wrong. Please try again later.',
+                        });
+                    }
+                });
+            }
+        });
+    });
 //
 // });
 
