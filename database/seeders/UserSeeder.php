5aac5d5e5eb6ee1e6b3950acc41b6b64ac747672
ahmed.othman <Ahmed.Othman@eg.aictec.com>
2025-03-27 18:46:47 +0200

    sitemap

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Afandina Admin',
             'email' => 'admin@afandina.com',
-            'password' => Hash::make('password'),
+            'password' => Hash::make('@fandina&#865'),
         ]);
     }
 }
